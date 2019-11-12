0ad5baaf9821166654c85f12fff70c61478e0aa5
Wesley Schneider <wesleyschneideraires@gmail.com>
2019-11-12 11:40:31 -0300

    feat: insert and list repos

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,36 +1,91 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {Keyboard, ActivityIndicator} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import api from '~/services/api';
+import getRealm from '~/services/realm';
 
 import Repository from '~/components/Repository';
 
 import {Container, Title, Form, Input, Submit, List} from './styles';
 
 export default function Main() {
+  const [input, setInput] = useState('');
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    async function loadRepositories() {
+      const realm = await getRealm();
+
+      const data = realm.objects('Repository').sorted('stars', true);
+
+      setRepositories(data);
+    }
+
+    loadRepositories();
+  }, []);
+
+  async function saveRepository(repo) {
+    const data = {
+      id: repo.id,
+      name: repo.name,
+      fullName: repo.full_name,
+      description: repo.description,
+      stars: repo.stargazers_count,
+      forks: repo.forks_count,
+    };
+
+    const realm = await getRealm();
+
+    realm.write(() => {
+      realm.create('Repository', data, 'modified');
+    });
+  }
+
+  async function handleSubmit() {
+    try {
+      setLoading(true);
+
+      const response = await api.get(`/repos/${input}`);
+      await saveRepository(response.data);
+
+      setInput('');
+      setError(false);
+      setLoading(false);
+
+      Keyboard.dismiss();
+    } catch (err) {
+      setError(true);
+      setLoading(false);
+      console.tron.log(err);
+    }
+  }
+
   return (
     <Container>
       <Title>Repositórios</Title>
       <Form>
         <Input
+          onChangeText={setInput}
           autoCapitalize="none"
           autoCorrect={false}
           placeholder="Procurar repositório"
+          error={error}
+          editable={!loading}
         />
-        <Submit onPress={() => {}}>
-          <Icon name="add" size={22} color="#fff" />
+        <Submit onPress={handleSubmit}>
+          {loading ? (
+            <ActivityIndicator color="#fff" size="small" />
+          ) : (
+            <Icon name="add" size={22} color="#fff" />
+          )}
         </Submit>
       </Form>
       <List
         keyboardShouldPersistTaps="handled"
-        data={[
-          {
-            id: 1,
-            name: 'Unform',
-            stars: 354,
-            forks: 95,
-            description:
-              'ReactJS form library to create uncontrolled form structures with nested fields, validations and much more!',
-          },
-        ]}
+        data={repositories}
         keyExtractor={item => String(item.id)}
         renderItem={({item}) => <Repository data={item} />}
       />
