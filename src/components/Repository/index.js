7f300fe315e3cbf7a1fabe435601d3185dd29256
Wesley Schneider <wesleyschneideraires@gmail.com>
2019-11-12 09:26:19 -0300

    chore: repository item

--- src/components/Repository/index.js
+++ src/components/Repository/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
-import {View} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
-// import { Container } from './styles';
+import {Container, Name, Description, Stats, Stat, StarCount} from './styles';
 
-export default function Repository() {
-  return <View />;
+export default function Repository({data}) {
+  return (
+    <Container>
+      <Name>{data.name}</Name>
+      <Description>{data.description}</Description>
+
+      <Stats>
+        <Stat>
+          <Icon name="star" size={16} color="#333" />
+          <StarCount>{data.stars}</StarCount>
+        </Stat>
+        <Stat>
+          <Icon name="code-fork" size={16} color="#333" />
+          <StarCount>{data.forks}</StarCount>
+        </Stat>
+      </Stats>
+    </Container>
+  );
 }
---
+++ src/components/Repository/styles.js
@@ -0,0 +1,38 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  padding: 20px;
+  margin-bottom: 15px;
+  background-color: #fff;
+  border-radius: 4px;
+  flex: 1;
+`;
+
+export const Name = styled.Text`
+  font-size: 18px;
+  font-weight: bold;
+  color: #333;
+`;
+
+export const Description = styled.Text.attrs({
+  numberOfLines: 2,
+})`
+  color: #666;
+  line-height: 20px;
+  margin-top: 5px;
+`;
+
+export const Stats = styled.View`
+  flex-direction: row;
+  margin-top: 15px;
+`;
+
+export const Stat = styled.View`
+  flex-direction: row;
+  align-items: center;
+  margin-right: 15px;
+`;
+
+export const StarCount = styled.Text`
+  margin-left: 6px;
+`;
